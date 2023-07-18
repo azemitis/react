7b674ab2156ac4821cf2280c9ed60eb967b71650
azemitis <7-days@inbox.lv>
2023-07-18 18:23:38 +0300

    First version

--- app/page.tsx
+++ app/page.tsx
@@ -25,73 +25,88 @@ export default function Home() {
     };
 
     return (
-        <div>
-            <div>
-                <label htmlFor="nameInput">Name:</label>
-                <input
-                    type="text"
-                    id="nameInput"
-                    value={name}
-                    onChange={handleNameChange}
-                    style={{
-                        padding: "8px",
-                        border: "1px solid #ccc",
-                        borderRadius: "4px",
-                        fontSize: "16px",
-                        marginBottom: "10px"
-                    }}
-                />
-                <span>{name}</span>
+        <div style={{ display: "flex" }}>
+            <div style={{ flex: 1 }}>
+                <h2>CV</h2>
+                <div style={{ marginBottom: "10px" }}>
+                    <label htmlFor="nameInput">Name:</label>
+                    <input
+                        type="text"
+                        id="nameInput"
+                        value={name}
+                        onChange={handleNameChange}
+                        style={{
+                            padding: "8px",
+                            border: "1px solid #ccc",
+                            borderRadius: "4px",
+                            fontSize: "16px",
+                            marginBottom: "10px"
+                        }}
+                    />
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                    <label htmlFor="emailInput">Email:</label>
+                    <input
+                        type="email"
+                        id="emailInput"
+                        value={email}
+                        onChange={handleEmailChange}
+                        style={{
+                            padding: "8px",
+                            border: "1px solid #ccc",
+                            borderRadius: "4px",
+                            fontSize: "16px",
+                            marginBottom: "10px"
+                        }}
+                    />
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                    <label htmlFor="phoneInput">Phone:</label>
+                    <input
+                        type="tel"
+                        id="phoneInput"
+                        value={phone}
+                        onChange={handlePhoneChange}
+                        style={{
+                            padding: "8px",
+                            border: "1px solid #ccc",
+                            borderRadius: "4px",
+                            fontSize: "16px",
+                            marginBottom: "10px"
+                        }}
+                    />
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                    <label htmlFor="experienceInput">Experience:</label>
+                    <textarea
+                        id="experienceInput"
+                        value={experience}
+                        onChange={handleExperienceChange}
+                        style={{
+                            padding: "8px",
+                            border: "1px solid #ccc",
+                            borderRadius: "4px",
+                            fontSize: "16px",
+                            marginBottom: "10px",
+                            height: "200px"
+                        }}
+                    />
+                </div>
             </div>
-            <div>
-                <label htmlFor="emailInput">Email:</label>
-                <input
-                    type="email"
-                    id="emailInput"
-                    value={email}
-                    onChange={handleEmailChange}
-                    style={{
-                        padding: "8px",
-                        border: "1px solid #ccc",
-                        borderRadius: "4px",
-                        fontSize: "16px",
-                        marginBottom: "10px"
-                    }}
-                />
-                <span>{email}</span>
-            </div>
-            <div>
-                <label htmlFor="phoneInput">Phone:</label>
-                <input
-                    type="tel"
-                    id="phoneInput"
-                    value={phone}
-                    onChange={handlePhoneChange}
-                    style={{
-                        padding: "8px",
-                        border: "1px solid #ccc",
-                        borderRadius: "4px",
-                        fontSize: "16px",
-                        marginBottom: "10px"
-                    }}
-                />
-                <span>{phone}</span>
-            </div>
-            <div>
-                <label htmlFor="experienceInput">Experience:</label>
-                <textarea
-                    id="experienceInput"
-                    value={experience}
-                    onChange={handleExperienceChange}
-                    style={{
-                        padding: "8px",
-                        border: "1px solid #ccc",
-                        borderRadius: "4px",
-                        fontSize: "16px",
-                        marginBottom: "10px"
-                    }}
-                />
-                <span>{experience}</span>
+            <div style={{ flex: 1, marginLeft: "20px" }}>
+                <h2>CV Preview</h2>
+                <div style={{ marginBottom: "10px" }}>
+                    <strong>Name:</strong> {name}
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                    <strong>Email:</strong> {email}
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                    <strong>Phone:</strong> {phone}
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                    <strong>Experience:</strong> {experience}
+                </div>
             </div>
         </div>
     );
