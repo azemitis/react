9cbcb6bcd0ec16d686bb2a9397f10dcdc0de5ac1
azemitis <7-days@inbox.lv>
2023-07-24 13:39:09 +0300

    All input fields

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,18 @@ export default function Home() {
     const [profession, setProfession] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
     const [profile, setProfile] = useState("");
-    const [company, setCompany] = useState("");
-    const [jobTitle, setJobTitle] = useState("");
-    const [date, setDate] = useState("");
-    const [description, setDescription] = useState("");
+    const [experiences, setExperiences] = useState([
+        {title: "", company: "", date: "", description: ""},
+    ]);
+
+    type Experience = {
+        title: string;
+        company: string;
+        date: string;
+        description: string;
+    };
 
     const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value);
@@ -30,32 +37,50 @@ export default function Home() {
         setPhone(event.target.value);
     };
 
+    const handleAddressChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setAddress(event.target.value);
+    };
+
     const handleProfileChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
         setProfile(event.target.value);
     };
 
-    const handleCompanyChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setCompany(event.target.value);
+    const handleExperienceChange = (
+        index: number,
+        key: keyof Experience,
+        value: string
+    ) => {
+        setExperiences((prevExperiences) => {
+            const newExperiences = [...prevExperiences];
+            newExperiences[index] = {
+                ...newExperiences[index],
+                [key]: value,
+            };
+            return newExperiences;
+        });
     };
 
-    const handleJobTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setJobTitle(event.target.value);
+    const handleAddExperience = () => {
+        setExperiences((prevExperiences) => [
+            ...prevExperiences,
+            {title: "", company: "", date: "", description: ""},
+        ]);
     };
 
-    const handleDateChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setDate(event.target.value);
-    };
-
-    const handleDescriptionChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-        setDescription(event.target.value);
+    const handleRemoveExperience = (index: number) => {
+        setExperiences((prevExperiences) =>
+            prevExperiences.filter((_, i) => i !== index)
+        );
     };
 
     return (
         <div style={{display: "flex"}}>
             <div style={{flex: 1}}>
-                <h2>Input fields</h2>
 
                 {/*Input section*/}
+                <h2>Input fields</h2>
+
+                {/* Add main fields */}
                 <div style={{marginBottom: "10px"}}>
                     <label htmlFor="nameInput">Name: </label>
                     <input
@@ -125,6 +150,23 @@ export default function Home() {
                 </div>
 
                 <div style={{marginBottom: "10px"}}>
+                    <label htmlFor="addressInput">Address: </label>
+                    <input
+                        type="address"
+                        id="addressInput"
+                        value={address}
+                        onChange={handleAddressChange}
+                        style={{
+                            padding: "8px",
+                            border: "1px solid #ccc",
+                            borderRadius: "4px",
+                            fontSize: "16px",
+                            marginBottom: "10px"
+                        }}
+                    />
+                </div>
+
+                <div style={{marginBottom: "10px"}}>
                     <label htmlFor="experienceInput">Profile: </label>
                     <textarea
                         id="profileInput"
@@ -136,89 +178,110 @@ export default function Home() {
                             borderRadius: "4px",
                             fontSize: "16px",
                             marginBottom: "10px",
-                            height: "200px"
+                            height: "100px"
                         }}
                     />
                 </div>
 
-                <div style={{marginBottom: "10px"}}>
-                    <label htmlFor="jobTitleInput">Title: </label>
-                    <input
-                        type="title"
-                        id="titleInput"
-                        value={jobTitle}
-                        onChange={handleJobTitleChange}
-                        style={{
-                            padding: "8px",
-                            border: "1px solid #ccc",
-                            borderRadius: "4px",
-                            fontSize: "16px",
-                            marginBottom: "10px"
-                        }}
-                    />
-                </div>
+                {/* Add multiple fields for many jobs */}
+                {experiences.map((experience, index) => (
+                    <div key={index}>
+                        <div style={{marginBottom: "10px"}}>
+                            <label htmlFor="titleInput">Title:</label>
+                            <input
+                                type="text"
+                                id="titleInput"
+                                value={experience.title}
+                                onChange={(e) =>
+                                    handleExperienceChange(index, "title", e.target.value)
+                                }
+                                style={{
+                                    padding: "8px",
+                                    border: "1px solid #ccc",
+                                    borderRadius: "4px",
+                                    fontSize: "16px",
+                                    marginBottom: "10px",
+                                }}
+                            />
+                        </div>
 
-                <div style={{marginBottom: "10px"}}>
-                    <label htmlFor="companyInput">Company: </label>
-                    <input
-                        type="company"
-                        id="companyInput"
-                        value={company}
-                        onChange={handleCompanyChange}
-                        style={{
-                            padding: "8px",
-                            border: "1px solid #ccc",
-                            borderRadius: "4px",
-                            fontSize: "16px",
-                            marginBottom: "10px"
-                        }}
-                    />
-                </div>
+                        <div style={{marginBottom: "10px"}}>
+                            <label htmlFor="companyInput">Company:</label>
+                            <input
+                                type="company"
+                                id="companyInput"
+                                value={experience.company}
+                                onChange={(e) =>
+                                    handleExperienceChange(index, "company", e.target.value)
+                                }
+                                style={{
+                                    padding: "8px",
+                                    border: "1px solid #ccc",
+                                    borderRadius: "4px",
+                                    fontSize: "16px",
+                                    marginBottom: "10px",
+                                }}
+                            />
+                        </div>
 
-                <div style={{marginBottom: "10px"}}>
-                    <label htmlFor="dateInput">Date: </label>
-                    <input
-                        type="date"
-                        id="dateInput"
-                        value={date}
-                        onChange={handleDateChange}
-                        style={{
-                            padding: "8px",
-                            border: "1px solid #ccc",
-                            borderRadius: "4px",
-                            fontSize: "16px",
-                            marginBottom: "10px"
-                        }}
-                    />
-                </div>
+                        <div style={{marginBottom: "10px"}}>
+                            <label htmlFor="dateInput">Date:</label>
+                            <input
+                                type="date"
+                                id="dateInput"
+                                value={experience.date}
+                                onChange={(e) =>
+                                    handleExperienceChange(index, "date", e.target.value)
+                                }
+                                style={{
+                                    padding: "8px",
+                                    border: "1px solid #ccc",
+                                    borderRadius: "4px",
+                                    fontSize: "16px",
+                                    marginBottom: "10px",
+                                }}
+                            />
+                        </div>
 
-                <div style={{marginBottom: "10px"}}>
-                    <label htmlFor="experienceInput">Description: </label>
-                    <textarea
-                        id="descriptionInput"
-                        value={description}
-                        onChange={handleDescriptionChange}
-                        style={{
-                            padding: "8px",
-                            border: "1px solid #ccc",
-                            borderRadius: "4px",
-                            fontSize: "16px",
-                            marginBottom: "10px",
-                            height: "200px"
-                        }}
-                    />
-                </div>
+                        <div style={{marginBottom: "10px"}}>
+                            <label htmlFor="experienceInput">Description</label>
+                            <textarea
+                                id="descriptionInput"
+                                value={experience.description}
+                                onChange={(e) =>
+                                    handleExperienceChange(index, "description", e.target.value)
+                                }
+                                style={{
+                                    padding: "8px",
+                                    border: "1px solid #ccc",
+                                    borderRadius: "4px",
+                                    fontSize: "16px",
+                                    marginBottom: "10px",
+                                    height: "100px",
+                                }}
+                            />
+                        </div>
+
+                        <button onClick={() => handleRemoveExperience(index)}>
+                            Remove Experience
+                        </button>
+                    </div>
+                ))}
+
+                <button onClick={handleAddExperience}>Add Experience</button>
             </div>
 
             {/*Display input*/}
             <div style={{flex: 1, marginLeft: "20px"}}>
-                <h2>CV Preview</h2>
+
+                {/* Main CV info */}
                 <div style={{marginBottom: "10px"}}>
                     <strong>Name:</strong> {name}
                 </div>
                 <div style={{marginBottom: "10px"}}>
                     <strong>Profession:</strong> {profession}
                 </div>
+                <h3>Details</h3>
                 <div style={{marginBottom: "10px"}}>
                     <strong>Email:</strong> {email}
                 </div>
@@ -226,22 +289,34 @@ export default function Home() {
                     <strong>Phone:</strong> {phone}
                 </div>
                 <div style={{marginBottom: "10px"}}>
+                    <strong>Address:</strong> {address}
+                </div>
+                <div style={{marginBottom: "10px"}}>
                     <strong>Profile:</strong> {profile}
                 </div>
-                <div style={{marginBottom: "10px"}}>
-                    <strong>Job Title:</strong> {jobTitle}
-                </div>
-                <div style={{marginBottom: "10px"}}>
-                    <strong>Company:</strong> {company}
-                </div>
-                <div style={{marginBottom: "10px"}}>
-                    <strong>Date:</strong> {date}
-                </div>
-                <div style={{marginBottom: "10px"}}>
-                    <strong>Description:</strong> {description}
-                </div>
-            </div>
 
+                {/* Display multiple jobs */}
+                <div>
+                    {experiences.map((experience, index) => (
+                        <div key={index}>
+                            <h3>Job {index + 1}</h3>
+                            <div>
+                                <strong>Title:</strong> {experience.title}
+                            </div>
+                            <div>
+                                <strong>Company:</strong> {experience.company}
+                            </div>
+                            <div>
+                                <strong>Date:</strong> {experience.date}
+                            </div>
+                            <div>
+                                <strong>Description:</strong> {experience.description}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
         </div>
     );
 }
